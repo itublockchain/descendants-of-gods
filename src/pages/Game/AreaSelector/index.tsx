48c0c8ec854f50b36a7fb3e70aeeb448b43e3a53
asgarovf <farhad@getwhelp.com>
2021-12-04 02:09:42 +0300

    style: area selection

--- src/pages/Game/AreaSelector/index.tsx
+++ src/pages/Game/AreaSelector/index.tsx
@@ -1,10 +1,17 @@
 import styles from "./AreaSelector.module.scss";
 import SmallImage from "assets/images/area-selector/small-map.png";
+import MapCropped from "assets/images/area-selector/map-cropped.png";
+import { Dispatch, SetStateAction } from "react";
 
-function AreaSelector({ setAreaSelected }) {
+type AreaSelectorProps = {
+  setAreaSelected: Dispatch<SetStateAction<boolean>>;
+};
+
+function AreaSelector({ setAreaSelected }: AreaSelectorProps) {
   return (
     <div className={styles.wrapper}>
       <div className={styles["map-container"]}>
+        <img className={styles.mapCropped} src={MapCropped} />
         <img
           src={SmallImage}
           className={styles["small-map"]}
